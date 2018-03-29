1fdb442667c73cf167ac44cc782ff48c8c9bedf8
mleker <nastya.z@grabr.io>
2018-03-29 18:54:53 +0300

    Post-review fixes

--- js/main.js
+++ js/main.js
@@ -1,63 +1,41 @@
-const ALT = 18;
-const LEFT_ARROW = 37;
-const RIGHT_ARROW = 39;
-
-const leftKeysCombo = [ALT, LEFT_ARROW];
-const rightKeysCombo = [ALT, RIGHT_ARROW];
-
 const mainCentral = document.querySelector(`.central`);
 const frames = document.querySelectorAll(`template`);
-
 let frameNumber = 0;
 
-let showFrameByNumber = (i) => {
-  mainCentral.innerHTML = ``;
-  mainCentral.appendChild(frames[i].content.cloneNode(true));
+const getFrameByNumber = (i) => {
+  return frames[i].content.cloneNode(true);
 };
 
-showFrameByNumber(0);
-
-let arrayContainsArray = ((superset, subset) => {
-  for (let value of subset) {
-    if (!superset.includes(value)) {
-      return false;
-    }
+const renderFrame = (frame) => {
+  while (mainCentral.firstChild) {
+    mainCentral.removeChild(mainCentral.firstChild);
   }
-  return true;
-});
+  mainCentral.appendChild(frame);
+};
 
-function showFramesOnKeysCombo() {
-  let pressedKeys = [];
-
-  document.onkeydown = (evt) => {
-    evt = evt || window.event;
-    pressedKeys.push(evt.keyCode);
-    if (arrayContainsArray(pressedKeys, leftKeysCombo)) {
-      showPrevTemplate();
-    } else if (arrayContainsArray(pressedKeys, rightKeysCombo)) {
-      showNextTemplate();
-    }
-  };
-
-  document.onkeyup = () => {
-    pressedKeys = [];
-  };
-}
-
-let showNextTemplate = () => {
+const showNextTemplate = () => {
   if (frameNumber < frames.length - 1) {
     frameNumber++;
-    showFrameByNumber(frameNumber);
+    renderFrame(getFrameByNumber(frameNumber));
   }
 };
 
-let showPrevTemplate = () => {
+const showPrevTemplate = () => {
   if (frameNumber > 0) {
     frameNumber--;
-    showFrameByNumber(frameNumber);
+    renderFrame(getFrameByNumber(frameNumber));
   }
 };
 
-showFramesOnKeysCombo();
+renderFrame(getFrameByNumber(0));
 
-
+document.onkeydown = (evt) => {
+  evt = evt || window.event;
+  if (evt.altKey) {
+    if (evt.key === `ArrowLeft`) {
+      showPrevTemplate();
+    } else if (evt.key === `ArrowRight`) {
+      showNextTemplate();
+    }
+  }
+};
